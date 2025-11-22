d7a870da2161a8e00f29489b53b7a70bbe4bfe43
mariosergio2025 <mariosergio.geraldo@gmail.com>
2025-11-22 18:06:02 -0300

    Exercicio 4

---
+++ Exercicio04_Reverse_String/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Exercicio04_Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reverse String");
+            Console.WriteLine("-------------");
+            Console.WriteLine("Qual palavra deseja reverte: ");
+            string palavra = Console.ReadLine();
+
+            string troca = "";
+            char[] arrays = new char[palavra.Length];
+
+            int contador = 0;           
+            while(contador < palavra.Length)
+            {
+                arrays[contador]=palavra[contador];
+                contador++;
+            }
+            contador = palavra.Length-1;
+            Console.WriteLine(contador);
+            string palavraReverse = "";
+            Console.Write("{ ");
+            while (contador >= 0)
+            {
+                if (contador ==0)
+                {
+                    Console.Write(arrays[contador] + " }");
+                }
+                else
+                {
+                    Console.Write(arrays[contador] + ", ");
+                }
+                    
+                palavraReverse = palavraReverse + arrays[contador];
+                contador--;
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine($"Palavra reverse é {palavraReverse}");
+
+
+
+        }
+    }
+}
